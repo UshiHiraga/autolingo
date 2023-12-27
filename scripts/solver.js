84cde9b448ba3a066fb40d65a9664c036adf5c77
Ushi Hiraga <ushi.hiraga@gmail.com>
2023-12-27 07:10:03 -0500

    Returned code for toggle 'use keyboard' button

--- scripts/solver.js
+++ scripts/solver.js
@@ -7,11 +7,11 @@ class DuolingoChallenge {
 
         // make sure the keyboard is enabled so we can paste in the input box
         // if (!this.challenge_internals.browserSettings.typingEnabled) {
-        //     const enable_typing_node = Array.from(
-        //         document.querySelectorAll("div")
-        //     ).find((e) => {
-        //         return e.innerHTML.toLowerCase() === "use keyboard";
-        //     });
+        // const enable_typing_node = Array.from(
+        //     document.querySelectorAll("div")
+        // ).find((e) => {
+        //     return e.innerHTML.toLowerCase() === "use keyboard";
+        // });
 
         //     enable_typing_node?.click();
         // }
