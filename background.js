c26f23a114cb8478c36670aa666b7f157585117d
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-02-14 13:40:58 -0500

    Removed legacy code for tooltip icon color.

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
-function handleInstall() {
+chrome.runtime.onInstalled.addListener(function () {
     console.log("The extension has been installed.");
-    chrome.storage.local.set({ autolingo_enabled: true });
-}
-
-function handleStorage(changes, namespace) {
-    if (!changes["autolingo_enabled"]) return false;
-
-    let isEnabled = Boolean(changes.autolingo_enabled.newValue);
-    chrome.action.setBadgeText({ text: isEnabled ? "✓" : "X" });
-    chrome.action.setBadgeBackgroundColor({ color: isEnabled ? "#008000" : "#EC5053" });
-}
-
-chrome.storage.onChanged.addListener(handleStorage);
-chrome.runtime.onInstalled.addListener(handleInstall);
+});
